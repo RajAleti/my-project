2cfdf2d452dda34c51f7c3b96c5d4c48d6f423b6
Raja <rajaleti111.origin@gmail.com>
2022-05-13 21:07:07 -0400

    made few changes

--- TerraformFiles/main.tf
+++ TerraformFiles/main.tf
@@ -24,16 +24,5 @@ resource "azurerm_storage_container" "rg" {
   container_access_type = "private"
 }
 
-provider "azurerm" {
-    features {
-    subscription_id = var.subscription_id
-    client_id = var.client_id
-    client_secret_id = var.client_secret_id
-    tenent_id= var.tenent_id 
 
-    }
-
-   
-  
-}
 
