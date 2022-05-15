fab544584ea765c846263a615c27849cb70d0fcb
Raja <rajaleti111.origin@gmail.com>
2022-05-15 17:02:08 -0400

    gi

--- TerraformFiles/backend.tf
+++ TerraformFiles/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "MainRG"
+    resource_group_name  = "MainResourceGroup"
     storage_account_name = "azurestorageraja789"
     container_name       = "democontainer0"
     key                  = "demotf.terraform.tfstate"
