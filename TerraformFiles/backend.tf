90de355284879daa95fdba95883a363b58213f90
Raja <rajaleti111.origin@gmail.com>
2022-05-14 22:17:08 -0400

    final terraform code in branch

---
+++ TerraformFiles/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "MainRG"
+    storage_account_name = "azurestorageraja789"
+    container_name       = "democontainer0"
+    key                  = "demotf.terraform.tfstate"
+  }
+}
